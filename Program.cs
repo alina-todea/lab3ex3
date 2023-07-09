9e2ca78c1a87fe94fc1fc08cde588441c66f2eb3
alina-todea <137332096+alina-todea@users.noreply.github.com>
2023-07-09 10:39:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab3ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Scrieti un program care sa verifice daca un sir de caractere citit de la tastatura este sau nu palindrom.
+
+
+            Console.WriteLine("introduceti un sir de caractere");
+            string text = Console.ReadLine();
+
+            string reverseText = string.Empty;
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                reverseText += text[i];
+            }
+
+            if (text == reverseText)
+            {
+                Console.WriteLine("Sirul e Palindrom");
+            }
+            else
+            {
+                Console.WriteLine("Sirul nu e Palindrom");
+            }
+
+
+
+        }
+    }
+}
